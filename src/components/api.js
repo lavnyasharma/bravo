da10c047ffce775d8dd6291f2169bad2dbfaa120
Lavanya Sharma <86040864+lavnyasharma@users.noreply.github.com>
2024-10-02 21:22:24 +0530

    push

--- src/components/api.js
+++ src/components/api.js
@@ -3,12 +3,14 @@ import axios from 'axios';
 
 // Replace with your API Gateway URL
 const API_GATEWAY_URL = '/proxy/';
+const API_Key = "eZ82az4Aiu8Q9zrkJHZGyaP8vas8wdqG5WHXqTQv";
 
 // Function to send a POST request to fetch vehicle details
 const searchVehicle = async (registrationNumber) => {
     try {
         const response = await axios.post(API_GATEWAY_URL, { registrationNumber }, {
             headers: {
+                'x-api-key': API_Key,
                 'Content-Type': 'application/json',
                 'Access-Control-Allow-Origin':'*',
             },
